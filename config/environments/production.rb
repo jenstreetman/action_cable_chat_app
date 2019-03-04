b108a1c60e803f96b58dedc7781d3384f3e09611
Jen <jenstreetman@gmail.com>
2019-03-03 19:57:03 -0700

    Update production.rb file with action cable allowed request origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://aqueous-tor-30704.herokuapp.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'https://aqueous-tor-30704.herokuapp.com/' ]
+  # config.action_cable.allowed_request_origins = [ 'https://aqueous-tor-30704.herokuapp.com/', 'https://aqueous-tor-30704.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
